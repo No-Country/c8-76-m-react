d6d0db339cc3d31aab886bd81a5804130e6d5709
jaime1315321 <jamoncayop@unal.edu.co>
2022-11-15 07:37:41 -0500

    Segundo commit preparación antes de routear

---
+++ src/AppWallet.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const AppWallet = () => {
+  return (
+    <div>
+        <h1>AppWallet Principal</h1>    
+    </div>
+  )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import { AppWallet } from './AppWallet'
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <AppWallet/>
   </React.StrictMode>
 )
