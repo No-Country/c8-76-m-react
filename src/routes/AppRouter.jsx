f45b93910268e45650e626f877bb6424d62d2a2e
Diego Alexandre <elalexdgr@gmail.com>
2022-11-22 12:01:06 -0300

    agregando firebase

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,12 +1,13 @@
 import React from "react";
-import { BrowserRouter, HashRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Home } from "../components/Home";
 
 export const AppRouter = () => {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<h1>Home</h1>} />
+          <Route path="/" element={<Home />} />
           <Route path="login" element={<h1>Login</h1>} />
           <Route path="/auth/register" element={<h1>Register</h1>} />
         </Routes>
