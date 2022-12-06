4241ae78a371dda16784b0bdda66c87baf88befa
jaime1315321 <jamoncayop@unal.edu.co>
2022-12-05 23:28:07 -0500

    Servicios sin imagenes

--- src/components/app/Servicios.jsx
+++ src/components/app/Servicios.jsx
@@ -3,6 +3,8 @@ import { styled } from '@mui/material/styles';
 import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
+import styles from '../../styles/Servicios.module.css'
+import logo1 from '../../assets/logo1.png'
 
 
 const Item = styled(Paper)(({ theme }) => ({
@@ -15,8 +17,30 @@ const Item = styled(Paper)(({ theme }) => ({
 
 export default function Servicios() {
   return (
-  <div>
-    <Box sx={{ flexGrow: 1}}>   
+  <div className={styles.principal}>
+    <h3>Servicios</h3>
+    <div className={styles.secundario}>
+      
+      <br />
+      <h3>Recarga de celulares:</h3>
+      <div className={styles.iconos}>
+        
+        <img src={logo1} alt="" />
+        <img src={logo1} alt="" />
+        <img src={logo1} alt="" />
+        <img src={logo1} alt="" />
+      </div>
+
+      <h3>Pagar servicios:</h3>
+      <div className={styles.iconos}>
+        <img src={logo1} alt="" />
+        <img src={logo1} alt="" />
+        <img src={logo1} alt="" />
+        <img src={logo1} alt="" />
+      </div>
+    </div>
+
+    {/* <Box sx={{ flexGrow: 1}}>   
       <Grid container justifyContent="center"  rowSpacing={1} columnSpacing={{ xs: 1, md: 12 }}>
         <Grid item xs={2} md={3}>
           <Item>1</Item>
@@ -31,7 +55,7 @@ export default function Servicios() {
           <Item>4</Item>
         </Grid>
       </Grid>
-    </Box>
+    </Box> */}
 </div>
   );
 }
