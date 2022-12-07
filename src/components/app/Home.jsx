803435ae1571d770101196554d9ab4f2050b4d56
Diego Alexandre <elalexdgr@gmail.com>
2022-12-06 22:07:56 -0300

    commit de prueba

--- src/components/app/Home.jsx
+++ src/components/app/Home.jsx
@@ -9,20 +9,28 @@ function Home() {
     <>
       <div className={s.textCenter}>
         <section>
-
           <h2>Saldo:{user.saldo}</h2>
-
         </section>
         <section>
           <h3>Movimientos</h3>
-          <article className={s.article}>
+          {/* <article className={s.article}>
             <p>Edesur</p>
             <div>
               <p>Pago de Edesur</p>
               <p>30/11/2022</p>
             </div>
             <p>$100</p>
-          </article>
+          </article> */}
+          {user.movimientos.map((movimiento) => (
+            <article className={s.article}>
+              <p>{movimiento.nombre}</p>
+              <div>
+                <p>{movimiento.descripcion}</p>
+                <p>{movimiento.fecha}</p>
+              </div>
+              <p>${movimiento.monto}</p>
+            </article>
+          ))}
         </section>
       </div>
     </>
--- src/components/app/Transferencias.jsx
+++ src/components/app/Transferencias.jsx
@@ -2,6 +2,25 @@ import React from "react";
 
 const Transferencias = () => {
   return <div>Transferencias</div>;
+  /* los valores del input en un estado
+      setUser({...user, movimientos[...user.movimientos, {
+        nombre: input.value,
+        descripcion: input.value,
+        fecha: input.value,
+        monto: input.value
+      }]})
+
+      saveUser({...user, movimientos[...user.movimientos, {
+        nombre: input.value,
+        descripcion: input.value,
+        fecha: input.value,
+        monto: input.value
+      }]})
+
+
+  
+  
+  */
 };
 
 export default Transferencias;
