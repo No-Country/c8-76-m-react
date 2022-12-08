8ba332f9fb51265f866a51e3a68b58365d1022b1
Diego Alexandre <elalexdgr@gmail.com>
2022-12-08 01:40:52 -0300

    sacando boton de header landing

--- src/components/app/Transferencias.jsx
+++ src/components/app/Transferencias.jsx
@@ -68,25 +68,6 @@ const Transferencias = () => {
       <button className={styles.button}>Enviar</button>
     </form>
   );
-  /* los valores del input en un estado
-      setUser({...user, movimientos[...user.movimientos, {
-        nombre: input.value,
-        descripcion: input.value,
-        fecha: input.value,
-        monto: input.value
-      }]})
-
-      saveUser({...user, movimientos[...user.movimientos, {
-        nombre: input.value,
-        descripcion: input.value,
-        fecha: input.value,
-        monto: input.value
-      }]})
-
-
-  
-  
-  */
 };
 
 export default Transferencias;
